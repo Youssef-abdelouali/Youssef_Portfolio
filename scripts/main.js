6e92809cb540a29a53ba5f19f8e0913cea491267
Youssef-abdelouali <youssefabdelouali225@gmail.com>
2025-04-28 17:01:08 +0200

    first phase of personnal portfolio

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+// Dynamically set the current time
+const timeEl = document.getElementById("time-display");
+function updateTime() {
+  const now = new Date();
+  timeEl.textContent = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+}
+updateTime();
+setInterval(updateTime, 60000); // update every minute
+
+function goBack() {
+  window.history.back();
+}
